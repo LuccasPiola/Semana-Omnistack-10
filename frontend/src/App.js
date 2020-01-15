dd86ae4bfb007b02ec260ee0b066d66dad3fb78b
Luccas Piola <luccas.hcpiola@gmail.com>
2020-01-15 11:08:42 -0300

    HTML + CSS ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente pai passa o componente filho
+// Estado: Informações mantidas pelo componente (Imutabilidade)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Gitgub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Gitgub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Gitgub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
